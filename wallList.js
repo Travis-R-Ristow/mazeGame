6d0eec5b27531a83fafda0b344e60e02f31d9a79
TRAVIS RisTow <TRtheGreat19@gmail.com>
2018-11-02 11:57:14 -0700

    started wallSSL

--- wallList.js
+++ wallList.js
@@ -1,9 +1,26 @@
 class wallList{
-	constructor(head){
-		this.head = head;
+	constructor(firstNode){
+		this.head = firstNode;
+		this.tail = firstNode;
 	}
 
-	add(node){
-		
+	add(newNode){
+		this.tail.setPointer(newNode);
+		this.tail = newNode;
+	}
+
+	getHead(){
+		return this.head;
+	}
+		setHead(node){
+
+		}
+
+	showAllWalls(){
+		var runnerNode = this.head;
+		while(runnerNode.getPointer() != null){
+			console.log(runnerNode);
+			runnerNode = runnerNode.getPointer();
+		}
 	}
 }
